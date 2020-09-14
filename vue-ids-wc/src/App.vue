62a40cb378dabca75b74578918328e8e62f0dbe5
Eric Angeles <enrico.angeles@infor.com>
2020-09-14 23:46:04 +0800

    added for loop sample in tag

--- vue-ids-wc/src/App.vue
+++ vue-ids-wc/src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <IdsTagWc tagTitle="Tag Component" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import IdsTagWc from './components/Tag.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    IdsTagWc
   }
 }
 </script>
---
+++ vue-ids-wc/src/components/Tag.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Tag Component</h1>
+    <ids-layout-grid>
+      <ids-layout-column>
+        <ids-tag
+          v-for="(tag, index) in tagList"
+          :key="index"
+          :color="tag.color"
+        >{{ tag.label }}</ids-tag>
+      </ids-layout-column>
+    </ids-layout-grid>
+  </div>
+</template>
+
+<script>
+import "ids-enterprise-wc/ids-layout-grid/ids-layout-grid.js";
+import "ids-enterprise-wc/ids-label/ids-label.js";
+import "ids-enterprise-wc/ids-icon/ids-icon.js";
+import "ids-enterprise-wc/ids-tag/ids-tag.js";
+
+const data = [
+  { label: "Normal Tag", color: "" },
+  { label: "Secondary Tag", color: "secondary" },
+  { label: "Error Tag", color: "error" },
+  { label: "Success Tag", color: "success" },
+  { label: "Warning Tag", color: "caution" },
+  { label: "Custom Tag Color", color: "#EDE3FC" }
+];
+
+export default {
+  name: "IdsTagWc",
+  data: function() {
+    return {
+      tagList: [...data]
+    };
+  },
+  props: {
+    tagTitle: String
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
