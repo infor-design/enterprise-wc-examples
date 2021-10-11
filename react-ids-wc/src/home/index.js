432566b1e914cc21ce6f91b5cd3f9905b318b500
Andrii Varhanov <andrii.varhanov@gmail.com>
2021-10-11 12:42:15 +0300

    Add categories to homepage react-examples

--- react-ids-wc/src/home/index.js
+++ react-ids-wc/src/home/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { routes } from '../settings/routes';
+import { categories, routes } from '../settings/routes';
 
 import 'ids-enterprise-wc/components/ids-layout-grid';
 import 'ids-enterprise-wc/components/ids-block-grid';
@@ -13,28 +13,41 @@ import './example.css';
 
 const Home = () => {
   return (
-    <ids-layout-grid auto="true" gap="md">
+    <ids-layout-grid auto="true" gap="md" class="main-list">
       <ids-layout-grid-cell>
-        <ids-block-grid align="center">
-          {routes.map(({ route, title }) => (
-            <ids-block-grid-item key={route}>
-              <Link to={`/${route}`}>
-                <ids-card auto-height="true">
-                  <div slot="card-content" className="main-list">
-                    <ids-text
-                      type="h2"
-                      font-size="16"
-                      font-weight="bold"
-                      color="slate-100"
-                    >
-                      {title}
-                    </ids-text>
-                  </div>
-                </ids-card>
-              </Link>
-            </ids-block-grid-item>
-          ))}
-        </ids-block-grid>
+        {categories.map(category => (
+          <div key={category}>
+            <ids-text type="h1" font-size="24">
+              {category}
+            </ids-text>
+            <ids-layout-grid auto="true">
+              <ids-layout-grid-cell>
+                <ids-block-grid align="center">
+                  {routes
+                    .filter(route => route.category === category)
+                    .map(({ route, title }) => (
+                      <ids-block-grid-item key={route}>
+                        <Link to={`/${route}`}>
+                          <ids-card auto-height="true">
+                            <div slot="card-content">
+                              <ids-text
+                                type="h2"
+                                font-size="16"
+                                font-weight="bold"
+                                color="slate-100"
+                              >
+                                {title}
+                              </ids-text>
+                            </div>
+                          </ids-card>
+                        </Link>
+                      </ids-block-grid-item>
+                    ))}
+                </ids-block-grid>
+              </ids-layout-grid-cell>
+            </ids-layout-grid>
+          </div>
+        ))}
       </ids-layout-grid-cell>
     </ids-layout-grid>
   );
--- react-ids-wc/src/settings/routes.js
+++ react-ids-wc/src/settings/routes.js
@@ -1,304 +1,375 @@
+// Component categories appear on the homepage
+export const categories = [
+  'Form Inputs', // 0
+  'Navigation and Interaction', // 1
+  'Messages and Alerts', // 2
+  'Lists', // 3
+  'Layouts', // 4
+  'Charts and Visualizations', // 5
+  'Typography' // 6
+]
+
 // List of all pages with correspondant components
 // Used on homepage to render components list and in router to attach a component to a route
 export const routes = [
   {
     route: 'ids-about',
     name: 'IdsAbout',
-    title: 'About'
+    title: 'About',
+    category: categories[2]
   },
   {
     route: 'ids-accordion',
     name: 'IdsAccordion',
-    title: 'Accordion'
+    title: 'Accordion',
+    category: categories[4]
   },
   {
     route: 'ids-alert',
     name: 'IdsAlert',
-    title: 'Alert'
+    title: 'Alert',
+    category: categories[0]
   },
   {
     route: 'ids-badge',
     name: 'IdsBadge',
-    title: 'Badge'
+    title: 'Badge',
+    category: categories[1]
   },
   {
     route: 'ids-block-grid',
     name: 'IdsBlockgrid',
-    title: 'Block Grid'
+    title: 'Block Grid',
+    category: categories[4]
   },
   {
     route: 'ids-breadcrumb',
     name: 'IdsBreadcrumb',
-    title: 'Breadcrumb'
+    title: 'Breadcrumb',
+    category: categories[1]
   },
   {
     route: 'ids-button',
     name: 'IdsButton',
-    title: 'Button'
+    title: 'Button',
+    category: categories[1]
   },
   {
     route: 'ids-card',
     name: 'IdsCard',
-    title: 'Card'
+    title: 'Card',
+    category: categories[4]
   },
   {
     route: 'ids-checkbox',
     name: 'IdsCheckbox',
-    title: 'Checkbox'
+    title: 'Checkbox',
+    category: categories[0]
   },
   {
     route: 'ids-color',
     name: 'IdsColor',
-    title: 'Color'
+    title: 'Color',
+    category: categories[5]
   },
   {
     route: 'ids-color-picker',
     name: 'IdsColorPicker',
-    title: 'Color Picker'
+    title: 'Color Picker',
+    category: categories[0]
   },
   {
     route: 'ids-counts',
     name: 'IdsCounts',
-    title: 'Counts'
+    title: 'Counts',
+    category: categories[5]
   },
   {
     route: 'ids-data-grid',
     name: 'IdsDataGrid',
-    title: 'Data Grid'
+    title: 'Data Grid',
+    category: categories[3]
   },
   {
     route: 'ids-draggable',
     name: 'IdsDraggable',
-    title: 'Draggable'
+    title: 'Draggable',
+    category: categories[3]
   },
   {
     route: 'ids-expandable-area',
     name: 'IdsExpandableArea',
-    title: 'Expandable Area'
+    title: 'Expandable Area',
+    category: categories[4]
   },
   {
     route: 'ids-fieldset',
     name: 'IdsFieldset',
-    title: 'Fieldset'
+    title: 'Fieldset',
+    category: categories[4]
   },
   {
     route: 'ids-header',
     name: 'IdsHeader',
-    title: 'Header'
+    title: 'Header',
+    category: categories[1]
   },
   {
     route: 'ids-hyperlink',
     name: 'IdsHyperlink',
-    title: 'Hyperlink'
+    title: 'Hyperlink',
+    category: categories[6]
   },
   {
     route: 'ids-icon',
     name: 'IdsIcon',
-    title: 'Icon'
+    title: 'Icon',
+    category: categories[2]
   },
   {
     route: 'ids-input',
     name: 'IdsInput',
-    title: 'Input'
+    title: 'Input',
+    category: categories[0]
   },
   {
     route: 'ids-layout-grid',
     name: 'IdsLayoutGrid',
-    title: 'Layout Grid'
+    title: 'Layout Grid',
+    category: categories[4]
   },
   {
     route: 'ids-list-view',
     name: 'IdsListView',
-    title: 'List View'
+    title: 'List View',
+    category: categories[3]
   },
   {
     route: 'ids-loading-indicator',
     name: 'IdsLoadingIndicator',
-    title: 'Loading Indicator'
+    title: 'Loading Indicator',
+    category: categories[1]
   },
   {
     route: 'ids-mask',
     name: 'IdsMask',
-    title: 'Mask'
+    title: 'Mask',
+    category: categories[0]
   },
   {
     route: 'ids-menu',
     name: 'IdsMenu',
-    title: 'Menu'
+    title: 'Menu',
+    category: categories[1]
   },
   {
     route: 'ids-menu-button',
     name: 'IdsMenuButton',
-    title: 'Menu Button'
+    title: 'Menu Button',
+    category: categories[1]
   },
   {
     route: 'ids-message',
     name: 'IdsMessage',
-    title: 'Message'
+    title: 'Message',
+    category: categories[2]
   },
   {
     route: 'ids-modal',
     name: 'IdsModal',
-    title: 'Modal'
+    title: 'Modal',
+    category: categories[1]
   },
   {
     route: 'ids-notification-banner',
     name: 'IdsNotificationBanner',
-    title: 'Notification Banner'
+    title: 'Notification Banner',
+    category: categories[3]
   },
   {
     route: 'ids-pager',
     name: 'IdsPager',
-    title: 'Pager'
+    title: 'Pager',
+    category: categories[1]
   },
   {
     route: 'ids-popup',
     name: 'IdsPopup',
-    title: 'Popup'
+    title: 'Popup',
+    category: categories[4]
   },
   {
     route: 'ids-popup-menu',
     name: 'IdsPopupMenu',
-    title: 'Popup Menu'
+    title: 'Popup Menu',
+    category: categories[1]
   },
   {
     route: 'ids-progress-bar',
     name: 'IdsProgressBar',
-    title: 'Progress Bar'
+    title: 'Progress Bar',
+    category: categories[2]
   },
   {
     route: 'ids-progress-chart',
     name: 'IdsProgressChart',
-    title: 'Progress Chart'
+    title: 'Progress Chart',
+    category: categories[5]
   },
   {
     route: 'ids-radio',
     name: 'IdsRadio',
-    title: 'Radio'
+    title: 'Radio',
+    category: categories[0]
   },
   {
     route: 'ids-rating',
     name: 'IdsRating',
-    title: 'Rating'
+    title: 'Rating',
+    category: categories[5]
   },
   {
     route: 'ids-render-loop',
     name: 'IdsRenderLoop',
-    title: 'Render Loop'
+    title: 'Render Loop',
+    category: categories[1]
   },
   {
     route: 'ids-scroll-view',
     name: 'IdsScrollView',
-    title: 'Scroll View'
+    title: 'Scroll View',
+    category: categories[1]
   },
   {
     route: 'ids-skip-link',
     name: 'IdsSkipLink',
-    title: 'Skip Link'
+    title: 'Skip Link',
+    category: categories[1]
   },
   {
     route: 'ids-slider',
     name: 'IdsSlider',
-    title: 'Slider'
+    title: 'Slider',
+    category: categories[5]
   },
   {
     route: 'ids-spinbox',
     name: 'IdsSpinbox',
-    title: 'Spinbox'
+    title: 'Spinbox',
+    category: categories[0]
   },
   {
     route: 'ids-step-chart',
     name: 'IdsStepChart',
-    title: 'Step Chart'
+    title: 'Step Chart',
+    category: categories[1]
   },
   {
     route: 'ids-summary-field',
     name: 'IdsSummaryField',
-    title: 'Summary Field'
+    title: 'Summary Field',
+    category: categories[0]
   },
   {
     route: 'ids-swipe-action',
     name: 'IdsSwipeAction',
-    title: 'Swipe Action'
+    title: 'Swipe Action',
+    category: categories[4]
   },
   {
     route: 'ids-switch',
     name: 'IdsSwitch',
-    title: 'Switch'
+    title: 'Switch',
+    category: categories[0]
   },
   {
     route: 'ids-tabs',
     name: 'IdsTabs',
-    title: 'Tabs'
+    title: 'Tabs',
+    category: categories[1]
   },
   {
     route: 'ids-tag',
     name: 'IdsTag',
-    title: 'Tag'
+    title: 'Tag',
+    category: categories[1]
   },
   {
     route: 'ids-text',
     name: 'IdsText',
-    title: 'Text'
+    title: 'Text',
+    category: categories[6]
   },
   {
     route: 'ids-textarea',
     name: 'IdsTextarea',
-    title: 'Text Area'
+    title: 'Text Area',
+    category: categories[0]
   },
   {
     route: 'ids-theme-switcher',
     name: 'IdsThemeSwitcher',
-    title: 'Theme Switcher'
+    title: 'Theme Switcher',
+    category: categories[1]
   },
   {
     route: 'ids-toast',
     name: 'IdsToast',
-    title: 'Toast'
+    title: 'Toast',
+    category: categories[2]
   },
   {
     route: 'ids-toggle-button',
     name: 'IdsToggleButton',
-    title: 'Toggle Button'
+    title: 'Toggle Button',
+    category: categories[1]
   },
   {
     route: 'ids-toolbar',
     name: 'IdsToolbar',
-    title: 'Toolbar'
+    title: 'Toolbar',
+    category: categories[4]
   },
   {
     route: 'ids-tooltip',
     name: 'IdsTooltip',
-    title: 'Tooltip'
+    title: 'Tooltip',
+    category: categories[2]
   },
   {
     route: 'ids-tree',
     name: 'IdsTree',
-    title: 'Tree'
+    title: 'Tree',
+    category: categories[1]
   },
   {
     route: 'ids-trigger-field',
     name: 'IdsTriggerField',
-    title: 'Trigger Field'
+    title: 'Trigger Field',
+    category: categories[0]
   },
   {
     route: 'ids-upload',
     name: 'IdsUpload',
-    title: 'Upload'
+    title: 'Upload',
+    category: categories[0]
   },
   {
     route: 'ids-upload-advanced',
     name: 'IdsUploadAdvanced',
-    title: 'Upload Advanced'
+    title: 'Upload Advanced',
+    category: categories[0]
   },
   {
     route: 'ids-virtual-scroll',
     name: 'IdsVirtualScroll',
-    title: 'Virtual Scroll'
+    title: 'Virtual Scroll',
+    category: categories[1]
   },
   {
     route: 'ids-wizard',
     name: 'IdsWizard',
-    title: 'Wizard'
+    title: 'Wizard',
+    category: categories[1]
   }
 ];
