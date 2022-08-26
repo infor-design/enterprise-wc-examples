373849c4c1653a690cf02c159431a70feddf059c
jmacaluso711 <macaluso.john@gmail.com>
2022-08-26 15:18:07 -0400

    App menu functioning

--- angular-ids-wc/src/app/components/ids-app-menu/demos/example/example.component.ts
+++ angular-ids-wc/src/app/components/ids-app-menu/demos/example/example.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+// import avatarPlaceHolder from '../../../../assets/images/avatar-placeholder.jpg';
 @Component({
   selector: 'app-example',
   templateUrl: './example.component.html',
   styleUrls: ['./example.component.css']
 })
-export class ExampleComponent implements OnInit {
+export class ExampleComponent implements OnInit, AfterViewInit {
+  @ViewChild('avatarImage', { read: ElementRef }) avatarImage;
+  public visible: boolean;
 
-  constructor() { }
+  constructor(
+    private sanitizer: DomSanitizer
+  ) { }
 
   ngOnInit(): void {
     console.log('example init');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('afterViewInit');
+    // this.avatarImage.nativeElement.src = this.sanitizer
+    //   .bypassSecurityTrustResourceUrl(`data:image/png;base64, ${avatarPlaceHolder}`);
+  }
+
+  handleClick() {
+    this.visible = !this.visible;
   }
 
 }
