1496f2d68c251fcfce76da4a9659311755d87d58
jmacaluso711 <macaluso.john@gmail.com>
2022-08-29 14:36:52 -0400

    ids-menu-button disabled example

--- angular-ids-wc/src/app/components/ids-menu-button/demos/disabled/disabled.component.ts
+++ angular-ids-wc/src/app/components/ids-menu-button/demos/disabled/disabled.component.ts
@@ -1,16 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import IdsMenuButton from 'ids-enterprise-wc/components/ids-menu-button/ids-menu-button';
 
 @Component({
   selector: 'app-disabled',
   templateUrl: './disabled.component.html',
   styleUrls: ['./disabled.component.css']
 })
-export class DisabledComponent implements OnInit {
+export class DisabledComponent implements OnInit, AfterViewInit {
+  @ViewChild('menuBtnEl', { read: ElementRef }) menuBtnEl: IdsMenuButton;
+  public disabled: boolean;
+  public defaultOnOutsideClick: Function;
 
   constructor() { }
 
   ngOnInit(): void {
-    console.log('disabled init');
+    console.log('ids-menu-button disabled OnInit');
   }
 
+  ngAfterViewInit(): void {
+    this.defaultOnOutsideClick = this.menuBtnEl.nativeElement.menuEl.onOutsideClick;
+  }
+
+  handleDisabledCheck(e: Event | any) {
+    this.disabled = e.target.checked;
+  }
+
+  handleForceMenuCheck(e: Event | any) {
+    const checked = e.target.checked;
+    this.menuBtnEl.nativeElement.menuEl.keepOpen = checked;
+    console.log(checked);
+    if (checked) {
+      this.menuBtnEl.nativeElement.menuEl.show();
+      this.menuBtnEl.nativeElement.menuEl.onOutsideClick = () => {};
+    } else {
+      this.menuBtnEl.nativeElement.menuEl.onOutsideClick = this.defaultOnOutsideClick;
+    }
+  }
 }
