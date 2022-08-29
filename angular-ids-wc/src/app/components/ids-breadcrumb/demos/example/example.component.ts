dbd642395326bbe2daaf2bb815cd664786ec1b21
jmacaluso711 <macaluso.john@gmail.com>
2022-08-29 15:24:19 -0400

    ids-breadcrumb examples

--- angular-ids-wc/src/app/components/ids-breadcrumb/demos/example/example.component.ts
+++ angular-ids-wc/src/app/components/ids-breadcrumb/demos/example/example.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-example',
   templateUrl: './example.component.html',
   styleUrls: ['./example.component.css']
 })
-export class ExampleComponent implements OnInit {
+export class ExampleComponent implements OnInit, AfterViewInit {
+  @ViewChild('breadcrumb', { read: ElementRef }) breadcrumb;
 
   constructor() { }
 
   ngOnInit(): void {
-    console.log('example init');
+    console.log('example OnInit');
   }
 
+  ngAfterViewInit(): void {
+    this.breadcrumb.nativeElement.onBreadcrumbActivate = (targetEl: any, previousEl: any) => {
+      console.info(`New Breadcrumb: "${targetEl.textContent}"\nPrevious Breadcrumb: "${previousEl.textContent}"`);
+    };
+  }
 }
