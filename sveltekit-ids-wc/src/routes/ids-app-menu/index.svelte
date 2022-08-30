8824adf231bf3ac9b2b9c3b983e62d6af6f5d23e
Ed Coyle <ed.coyle@infor.com>
2022-08-30 13:46:08 -0400

    Add IdsAppMenu examples to Sveltekit Demoapp

---
+++ sveltekit-ids-wc/src/routes/ids-app-menu/index.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import IdsDemoListing from '../../components/demo/IdsDemoListing.svelte';
+
+  const listingData = [
+    {
+      link: 'example',
+      type: 'Main Example',
+      description: 'Simple usage of an IDS App Menu within Svelte'
+    }
+  ];
+</script>
+
+<ids-layout-grid auto="true">
+  <ids-text font-size="20" type="h1">Ids App Menu Examples</ids-text>
+</ids-layout-grid>
+
+<ids-layout-grid auto="true">
+  <ids-layout-grid-cell>
+    <IdsDemoListing data={listingData} componentName="ids-app-menu"></IdsDemoListing>
+  </ids-layout-grid-cell>
+</ids-layout-grid>
