29b4e71aa63fa6eaed8604dbf062d14abdea5cd0
Ed Coyle <ed.coyle@infor.com>
2022-08-30 14:11:17 -0400

    Add IdsAreaChart example to Sveltekit Demoapp

---
+++ sveltekit-ids-wc/src/routes/ids-area-chart/example.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { writable } from 'svelte/store';
+  import type { Writable } from 'svelte/store';
+
+  let ajaxItems: Writable<Array<{ id: string, name: string }>> = writable([]);
+
+  onMount(async (): Promise<void> => {
+    await import('ids-enterprise-wc/components/ids-area-chart/ids-area-chart');
+
+    // Populates store that populates the chart
+    const res = await fetch('/data/components');
+    const json = await res.json();
+    $ajaxItems = json;
+  });
+</script>
+
+<ids-layout-grid auto="true">
+  <ids-text font-size="12" type="h1">Area Chart (Imported into Svelte)</ids-text>
+</ids-layout-grid>
+<ids-layout-grid auto="true">
+  <ids-layout-grid-cell>
+    <ids-area-chart 
+      title="An area chart showing component usage" 
+      width="800" 
+      height="500" 
+      id="index-example"
+      data={$ajaxItems}></ids-area-chart>
+  </ids-layout-grid-cell>
+</ids-layout-grid>
