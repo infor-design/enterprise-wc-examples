02e0fef56c56d7a6f571b37ad933c1b08846116c
Ed Coyle <ed.coyle@infor.com>
2022-08-30 11:58:51 -0400

    Add IdsActionSheet example to Sveltekit demoapp

---
+++ sveltekit-ids-wc/src/routes/ids-action-sheet/index.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import IdsDemoListing from '../../components/demo/IdsDemoListing.svelte';
+
+  const listingData = [
+    {
+      link: 'example',
+      type: 'Main Example',
+      description: 'Simple usage of an IDS Action Sheet within Svelte'
+    }
+  ];
+</script>
+
+<ids-layout-grid auto="true">
+  <ids-text font-size="20" type="h1">Ids Action Sheet Examples</ids-text>
+</ids-layout-grid>
+
+<ids-layout-grid auto="true">
+  <ids-layout-grid-cell>
+    <IdsDemoListing data={listingData} componentName="ids-action-sheet"></IdsDemoListing>
+  </ids-layout-grid-cell>
+</ids-layout-grid>
