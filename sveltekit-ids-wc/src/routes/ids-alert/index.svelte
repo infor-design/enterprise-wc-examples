e842fd46457393fc3d12d0f6d4bc02f8ad228bef
Ed Coyle <ed.coyle@infor.com>
2022-08-30 12:05:58 -0400

    Add IdsAlert example to Sveltekit demoapp

---
+++ sveltekit-ids-wc/src/routes/ids-alert/index.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import IdsDemoListing from '../../components/demo/IdsDemoListing.svelte';
+
+  const listingData = [
+    {
+      link: 'example',
+      type: 'Main Example',
+      description: 'Simple usage of an IDS Alert within Svelte'
+    }
+  ];
+</script>
+
+<ids-layout-grid auto="true">
+  <ids-text font-size="20" type="h1">Ids Alert Examples</ids-text>
+</ids-layout-grid>
+
+<ids-layout-grid auto="true">
+  <ids-layout-grid-cell>
+    <IdsDemoListing data={listingData} componentName="ids-alert"></IdsDemoListing>
+  </ids-layout-grid-cell>
+</ids-layout-grid>
