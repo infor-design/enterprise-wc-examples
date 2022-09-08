4e30adc05e75854a2babc21e261b1bf6a0d8aecf
jmacaluso711 <macaluso.john@gmail.com>
2022-09-08 10:46:58 -0400

    Add ids-message example and event example

--- angular-ids-wc/src/app/components/ids-message/demos/example/example.component.ts
+++ angular-ids-wc/src/app/components/ids-message/demos/example/example.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-example',
   templateUrl: './example.component.html',
   styleUrls: ['./example.component.css']
 })
-export class ExampleComponent implements OnInit {
+export class ExampleComponent implements OnInit, AfterViewInit {
+  @ViewChild('messageError', { read: ElementRef }) messageError;
+  @ViewChild('messageErrorTrigger', { read: ElementRef }) messageErrorTrigger;
 
   constructor() { }
 
   ngOnInit(): void {
-    console.log('example init');
+    console.log('IdsMessage example OnInit');
   }
 
+  ngAfterViewInit(): void {
+    if (!this.messageError) {
+      return;
+    }
+    this.messageError.nativeElement.target = this.messageErrorTrigger.nativeElement;
+    this.messageError.nativeElement.triggerType = 'click';
+
+    this.messageError.nativeElement.onButtonClick = (buttonEl: any) => {
+      const response = buttonEl.cancel ? 'cancelled' : 'confirmed';
+      console.info(`IdsMessage was ${response}`);
+      this.messageError.nativeElement.hide();
+    };
+  }
+
+  handleBeforeShow() {
+    this.messageErrorTrigger.nativeElement.disabled = true;
+    return true;
+  }
+
+  handleHide() {
+    this.messageErrorTrigger.nativeElement.disabled = false;
+  }
 }
