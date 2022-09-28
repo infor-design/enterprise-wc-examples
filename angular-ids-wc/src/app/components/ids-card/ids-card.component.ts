ba29e3919ce7770386c89b3a619f204c2bbaab64
jmacaluso711 <macaluso.john@gmail.com>
2022-09-28 10:39:52 -0400

    ids-card scaffold

---
+++ angular-ids-wc/src/app/components/ids-card/ids-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ids-card',
+  templateUrl: './ids-card.component.html',
+  styleUrls: ['./ids-card.component.css']
+})
+export class IdsCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
