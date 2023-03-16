7bfdbb0c3528c89f3c779e13ffba22833eedf333
Clay Clecidor <clay.clecidor@infor.com>
2023-03-16 17:52:21 -0400

    847-react-typescript-examples (refactored with IdsGrid, IdsGridCell)

--- react-ts-ids-wc/src/examples/ids-input/index.tsx
+++ react-ts-ids-wc/src/examples/ids-input/index.tsx
@@ -144,7 +144,7 @@ const IdsInput = () => {
         </IdsGridCell>
       </IdsGrid>
 
-      <IdsGrid auto="true">
+      <IdsGrid auto>
         <IdsGridCell>
           <ids-text font-size="12" type="h2">
             Ids Input - Text align
