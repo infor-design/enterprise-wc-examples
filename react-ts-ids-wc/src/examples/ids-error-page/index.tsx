31e84775145a5b563891de4044a9f9718936b201
Clay Clecidor <clay.clecidor@infor.com>
2023-03-17 17:48:53 -0400

    847-react-typescript-examples modified:   src/examples/ids-error-page/index.tsx

--- react-ts-ids-wc/src/examples/ids-error-page/index.tsx
+++ react-ts-ids-wc/src/examples/ids-error-page/index.tsx
@@ -1,32 +1,29 @@
-import React, { useRef, useEffect } from 'react';
+import { useEffect, useRef } from 'react';
+import useEvent from '../../hooks/useEvent';
 import type IdsErrorPageType from 'ids-enterprise-wc/components/ids-error-page/ids-error-page';
 import 'ids-enterprise-wc/components/ids-error-page/ids-error-page';
 
 const IdsErrorPage = () => {
   const errorPageRef = useRef<IdsErrorPageType>();
 
+  const handleActionBtn = (e: any) => {
+    errorPageRef?.current?.hide?.()
+      .then?.((data) => console.log('error-modal hidden', data));
+  }
+
+  const handleBeforeHide = (e: any) => {
+    // passing false to response will cause hide() action to be "vetoed"
+    // e?.detail?.response?.(false);
+    e?.detail?.response?.(true);
+  }
+
+  useEvent('action-button', handleActionBtn, errorPageRef);
+  useEvent('beforehide', handleBeforeHide, errorPageRef);
+
   useEffect(() => {
-    const errorPage = errorPageRef.current;
-
     // Show on initial page load
-    errorPage?.show();
-
-    const handleActionBtn = () => {
-      errorPage?.hide();
-    }
-
-    const handleBeforeHide = (e: any) => {
-      e?.detail?.response?.(false);
-    }
-
-    errorPage?.addEventListener('action-button', handleActionBtn);
-    errorPage?.addEventListener('beforehide', handleBeforeHide);
-
-    return () => {
-      errorPage?.removeEventListener('action-button', handleActionBtn);
-      errorPage?.removeEventListener('beforehide', handleBeforeHide);
-    }
-  }, [])
+    errorPageRef?.current?.show?.();
+  }, []);
 
   return (
     <ids-error-page
@@ -35,7 +32,7 @@ const IdsErrorPage = () => {
       icon="empty-error-loading"
       label="Access Denied"
       description="Configuration Manager access has been denied."
-      button-text="Return"
+      button-text="Accept"
     ></ids-error-page>
   );
 };
