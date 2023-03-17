7bbf919c4a7a621fd3a0e696a60ff860f7e7af18
Clay Clecidor <clay.clecidor@infor.com>
2023-03-17 13:06:16 -0400

    847-react-typescript-examples (added src/components/ids-draggable/IdsDraggable.tsx)

--- react-ts-ids-wc/src/examples/ids-draggable/index.tsx
+++ react-ts-ids-wc/src/examples/ids-draggable/index.tsx
@@ -1,54 +1,58 @@
-import React, { useRef } from 'react';
-import { useEvent } from '../../hooks/useEvent';
+import IdsDraggable from '../../components/ids-draggable/IdsDraggable';
+import IdsGrid from '../../components/ids-grid/IdsGrid';
 import IdsTitle from '../../components/ids-title/IdsTitle';
-import type IdsDraggableType from 'ids-enterprise-wc/components/ids-draggable/ids-draggable';
-import 'ids-enterprise-wc/components/ids-draggable/ids-draggable';
+import './example.css';
 
 
-const IdsDraggable = () => {
-  const handleDragStart = (e: CustomEvent) => console.info('ids-dragstart', e.detail);
-  const handleDrag = (e: CustomEvent) => console.info('ids-drag', e.detail);
-  const handleDragEnd = (e: CustomEvent) => console.info('ids-dragend', e.detail);
+const DemoBox: React.FC<{ children: any; }> = (props) => (
+  <div className="ids-draggable-demo-box">{props.children}</div>
+);
 
-  const dragRef = useRef<IdsDraggableType>();
-  useEvent('ids-dragstart', handleDragStart, dragRef);
-  useEvent('ids-drag', handleDrag, dragRef);
-  useEvent('ids-dragend', handleDragEnd, dragRef);
+const IdsDraggableExample = () => {
+  const onDragStart = (e: any) => console.info('ids-dragstart', e?.detail);
+  const onDrag = (e: any) => console.info('ids-drag', e?.detail);
+  const onDragEnd = (e: any) => console.info('ids-dragend', e?.detail);
 
   return (
     <>
       <IdsTitle>Draggable Example</IdsTitle>
 
-      <ids-layout-grid auto="true">
-        <ids-draggable>
-          <div className="ids-draggable-demo-box">
-            <ids-text>no axis</ids-text>
-          </div>
-        </ids-draggable>
-        <ids-draggable axis="y">
-          <div className="ids-draggable-demo-box">
-            <ids-text>axis='y'</ids-text>
-          </div>
-        </ids-draggable>
-        <ids-draggable axis="x">
-          <div className="ids-draggable-demo-box">
-            <ids-text>axis='x'</ids-text>
-          </div>
-        </ids-draggable>
-        <ids-draggable
-          ref={dragRef}
-          id="limit-transform-draggable"
-          max-transform-x="80"
-          min-transform-x="-20"
-          max-transform-y="80"
-          min-transform-y="-80"
-          handle="#limit-transform-draggable .ids-draggable-demo-box"
+      <IdsGrid auto>
+        <IdsDraggable
+          onDragStart={onDragStart}
+          onDrag={onDrag}
+          onDragEnd={onDragEnd}
         >
-          <div className="ids-draggable-demo-box">
+          <DemoBox>
+            <ids-text>no axis</ids-text>
+          </DemoBox>
+        </IdsDraggable>
+        <IdsDraggable axis="y">
+          <DemoBox>
+            <ids-text>axis='y'</ids-text>
+          </DemoBox>
+        </IdsDraggable>
+        <IdsDraggable axis="x">
+          <DemoBox>
+            <ids-text>axis='x'</ids-text>
+          </DemoBox>
+        </IdsDraggable>
+        <IdsDraggable
+          id="limit-transform-draggable"
+          handle="#limit-transform-draggable .ids-draggable-demo-box"
+          maxTransformX={80}
+          minTransformX={-20}
+          maxTransformY={80}
+          minTransformY={-80}
+          onDragStart={onDragStart}
+          onDrag={onDrag}
+          onDragEnd={onDragEnd}
+        >
+          <DemoBox>
             <div>
               <ids-text>w/limits</ids-text>
             </div>
-          </div>
+          </DemoBox>
           <div className="draggable-limit-markers">
             <ids-text tag="p" color="unset" font-size="10">
               min-transform-x: -20
@@ -63,34 +67,34 @@ const IdsDraggable = () => {
               max-transform-y: 80
             </ids-text>
           </div>
-        </ids-draggable>
-      </ids-layout-grid>
-      <ids-layout-grid auto="true">
+        </IdsDraggable>
+      </IdsGrid>
+      <IdsGrid auto>
         <ids-text font-size="12" type="h1">
           Draggable (Bounded by Div/Container)
         </ids-text>
-      </ids-layout-grid>
-      <ids-layout-grid auto="true">
+      </IdsGrid>
+      <IdsGrid auto>
         <div className="ids-draggable-demo-content">
-          <ids-draggable parent-containment>
-            <div className="ids-draggable-demo-box">
+          <IdsDraggable parentContainment>
+            <DemoBox>
               <ids-text>no axis</ids-text>
-            </div>
-          </ids-draggable>
-          <ids-draggable axis="y" parent-containment>
-            <div className="ids-draggable-demo-box">
+            </DemoBox>
+          </IdsDraggable>
+          <IdsDraggable axis="y" parentContainment>
+            <DemoBox>
               <ids-text>axis='y'</ids-text>
-            </div>
-          </ids-draggable>
-          <ids-draggable axis="x" parent-containment>
-            <div className="ids-draggable-demo-box">
+            </DemoBox>
+          </IdsDraggable>
+          <IdsDraggable axis="x" parentContainment>
+            <DemoBox>
               <ids-text>axis='x'</ids-text>
-            </div>
-          </ids-draggable>
+            </DemoBox>
+          </IdsDraggable>
         </div>
-      </ids-layout-grid>
+      </IdsGrid>
     </>
   );
 };
 
-export default IdsDraggable;
+export default IdsDraggableExample;
