1ba0a49c2a8b77436b6972bd489dc9224227702b
Clay Clecidor <clay.clecidor@infor.com>
2023-03-17 11:52:20 -0400

    847-react-typescript-examples (added src/hooks/useEvent.ts)

--- react-ts-ids-wc/src/hooks/useEvent.ts
+++ react-ts-ids-wc/src/hooks/useEvent.ts
@@ -1,22 +1,23 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 
-type ReactUseRefType = ReturnType<typeof useRef<any>>;
+type ReactUseRefType<T> = ReturnType<typeof useRef<T>>;
 
-export function useEvent<T extends Event | CustomEvent>(
+export function useEvent<T extends Element>(
   eventName: string,
-  eventHandler: (event: T) => void,
-  modalRef?: ReactUseRefType
-): ReactUseRefType {
-  const fallbackRef = useRef<any>();
-  const ref = modalRef ?? fallbackRef;
+  eventHandler: (evt: Event) => void,
+  modalRef?: ReactUseRefType<T>
+): ReactUseRefType<T> {
 
-  // Event handler to be used in attach and cleanup event listener
-  const callback = useCallback((evt: Event) => eventHandler(evt as T), [eventHandler])
+  const fallbackRef = useRef<T>();
+  const ref = modalRef ?? fallbackRef;
 
   useEffect(() => {
     // Adding ref current element to variable to be able cleanup event listeners on unmount
     const idsElement = ref?.current;
+
+    // Event handler to be used in attach and cleanup event listener
+    const callback = (evt: Event) => eventHandler(evt);
 
     // Attach event listeners
     idsElement?.addEventListener(eventName, callback);
@@ -25,7 +26,7 @@ export function useEvent<T extends Event | CustomEvent>(
       // Clean event listeners on component unmount
       idsElement?.removeEventListener(eventName, callback);
     }
-  }, [eventName, callback, ref])
+  }, [eventName, eventHandler, ref])
 
   return ref;
 }
