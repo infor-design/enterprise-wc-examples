cfb86545a416181a73ce5f346968a560bd513d9c
Clay Clecidor <clay.clecidor@infor.com>
2023-03-17 18:54:40 -0400

    847-react-typescript-examples modified:   src/examples/ids-progress-bar/index.tsx

--- react-ts-ids-wc/src/examples/ids-progress-bar/index.tsx
+++ react-ts-ids-wc/src/examples/ids-progress-bar/index.tsx
@@ -1,8 +1,10 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useState } from 'react';
+import useEvent from '../../hooks/useEvent';
 import IdsGrid, { IdsGridCell } from '../../components/ids-grid/IdsGrid';
 import IdsTitle from '../../components/ids-title/IdsTitle';
 import type IdsProgressBarType from 'ids-enterprise-wc/components/ids-progress-bar/ids-progress-bar';
 import type IdsToggleButtonType from 'ids-enterprise-wc/components/ids-toggle-button/ids-toggle-button';
+import 'ids-enterprise-wc/components/ids-toggle-button/ids-toggle-button';
 import 'ids-enterprise-wc/components/ids-progress-bar/ids-progress-bar';
 
 
@@ -11,55 +13,45 @@ const IdsProgressBar = () => {
   const setValueRef = useRef<IdsToggleButtonType>();
   const setDisableRef = useRef<IdsToggleButtonType>();
   const setLabelRef = useRef<IdsToggleButtonType>();
+  const [origValue, setOrigValue] = useState('');
 
-  useEffect(() => {
-    // Adding ref current element to variable to be able cleanup event listeners on unmount
-    const btnUpdateVal = setValueRef.current;
-    const btnDisable = setDisableRef.current;
-    const btnSetLabel = setLabelRef.current;
+  // Event handler to be used in attach and cleanup event listener
+  // Update and reset value
+  const handleUpdateVal = (e: any) => {
     const element = progressRef.current as IdsProgressBarType;
 
     // Get original value
-    const orgValue = element?.value;
+    if (!origValue) setOrigValue(element?.value ?? '');
 
-    // Event handler to be used in attach and cleanup event listener
-    // Update and reset value
-    const handleUpdateVal = (e: any) => {
-      if (element?.disabled) {
-        return;
-      }
-      e?.target?.toggle?.();
-      const max = '100';
-      if (element) element.value = element.value === max ? orgValue : max;
-    };
-
-    // Toggle disable/enable
-    const handleDisabled = (e: any) => {
-      e?.target?.toggle?.();
-      element.disabled = !element.disabled;
-    };
-
-    // Toggle label audible
-    const handleLabel = (e: any) => {
-      if (element.disabled) {
-        return;
-      }
-      e?.target?.toggle?.();
-      element.labelAudible = !element.labelAudible;
-    };
-
-    // Attach event listeners
-    btnUpdateVal?.addEventListener('click', handleUpdateVal);
-    btnDisable?.addEventListener('click', handleDisabled);
-    btnSetLabel?.addEventListener('click', handleLabel);
-
-    // Cleanup event listener on React component unmount
-    return function cleanup() {
-      btnUpdateVal?.removeEventListener('click', handleUpdateVal);
-      btnDisable?.removeEventListener('click', handleDisabled);
-      btnSetLabel?.removeEventListener('click', handleLabel);
+    if (element?.disabled) {
+      return;
     }
-  }, []);
+
+    e?.target?.toggle?.();
+    const max = '100';
+    if (element) element.value = element.value === max ? origValue : max;
+  };
+
+  // Toggle disable/enable
+  const handleDisabled = (e: any) => {
+    const element = progressRef.current as IdsProgressBarType;
+    e?.target?.toggle?.();
+    element.disabled = !element.disabled;
+  };
+
+  // Toggle label audible
+  const handleLabel = (e: any) => {
+    const element = progressRef.current as IdsProgressBarType;
+    if (element?.disabled) {
+      return;
+    }
+    e?.target?.toggle?.();
+    element.labelAudible = !element.labelAudible;
+  };
+
+  useEvent('click', handleUpdateVal, setValueRef);
+  useEvent('click', handleDisabled, setDisableRef);
+  useEvent('click', handleLabel, setLabelRef);
 
   return (
     <>
