230a174390b45b1102eaab018851c70a750e797f
Clay Clecidor <clay.clecidor@infor.com>
2023-10-31 16:31:00 -0400

    1505-angular-form-fields-ng-model renamed:    src/app/shared/directives/ids-form-accessors.module.ts -> src/directives/ids-form-accessors.module.ts renamed:    src/app/shared/directives/ids-form-accessors.ts -> src/directives/ids-form-accessors.ts

--- angular-ids-wc/src/app/components/ids-forms/ids-forms.module.ts
+++ angular-ids-wc/src/app/components/ids-forms/ids-forms.module.ts
@@ -2,7 +2,7 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { IdsFormAccessorsModule } from 'src/app/shared/directives/ids-form-accessors.module';
+import { IdsFormAccessorsModule } from 'src/directives/ids-form-accessors.module';
 import { IdsFormsRoutingModule } from './ids-forms-routing.module';
 import { IdsFormsComponent } from './ids-forms.component';
 import { ExampleComponent } from './demos/example/example.component';
