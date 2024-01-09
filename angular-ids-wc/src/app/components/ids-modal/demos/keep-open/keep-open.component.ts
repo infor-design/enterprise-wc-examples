1989f108d635717f599807226853883924b4a0f7
Ed Coyle <ed.coyle@infor.com>
2024-01-09 13:30:29 -0500

    Fix Angular Modal examples

--- angular-ids-wc/src/app/components/ids-modal/demos/keep-open/keep-open.component.ts
+++ angular-ids-wc/src/app/components/ids-modal/demos/keep-open/keep-open.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-keep-open',
   templateUrl: './keep-open.component.html',
   styleUrls: ['./keep-open.component.css']
 })
-export class KeepOpenComponent implements OnInit {
+export class KeepOpenComponent implements OnInit, AfterViewInit {
+  @ViewChild('modal', { read: ElementRef }) modal;
+  @ViewChild('closeOutsideCheck', { read: ElementRef }) closeOutsideCheck;
+
+  private defaultOnOutsideClick;
 
   constructor() { }
 
@@ -13,4 +17,25 @@ export class KeepOpenComponent implements OnInit {
     console.log('keep-open init');
   }
 
+  ngAfterViewInit(): void {
+    this.defaultOnOutsideClick = this.modal.nativeElement.onOutsideClick;
+  }
+
+  handleShow() {
+    this.modal.nativeElement.show();
+  }
+
+  handleHide() {
+    this.modal.nativeElement.hide();
+  }
+
+  handleClickOutsideChange(e: Event) {
+    const isChecked = (e.target as any).checked;
+    if (isChecked) {
+      this.modal.nativeElement.onOutsideClick = this.defaultOnOutsideClick;
+    } else {
+      // Overrides the default `onOutsideClick` to just keep the Modal open
+      this.modal.nativeElement.onOutsideClick = () => { };
+    }
+  }
 }
