cef6cdbfeea2ac92ac49f1e53a97e042b6d74bbf
Clay Clecidor <clay.clecidor@infor.com>
2024-03-22 02:39:47 -0400

    1658-ids-notification-banner-message-attribute

--- angular-ids-wc/src/app/components/ids-notification-banner/demos/dynamic/dynamic.component.ts
+++ angular-ids-wc/src/app/components/ids-notification-banner/demos/dynamic/dynamic.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-dynamic',
   templateUrl: './dynamic.component.html',
   styleUrls: ['./dynamic.component.css']
 })
-export class DynamicComponent implements OnInit {
+export class DynamicComponent implements OnInit, AfterViewInit {
+  @ViewChild('programmaticNotification', { read: ElementRef }) programmaticNotification;
 
+  dynamicText: string = 'Test dynamic text';
+  testBanner: string = 'Test banner text';
+  warningBanner: string = 'Warning Banner Text';
   notifications = [];
-  testBanner = 'Test banner text';
-  warningBanner = 'Warning Banner Text';
   filteredGridObjects = [];
 
   constructor() { }
@@ -23,7 +25,17 @@ export class DynamicComponent implements OnInit {
     });
     this.filteredGridObjects = this.notifications;
     this.testBanner = 'This is a test banner';
-    
+  }
+
+  ngAfterViewInit(): void {
+    console.log('example AfterViewInit', this.programmaticNotification);
+
+    this.programmaticNotification?.nativeElement?.add({
+      id: 'ids-notification-banner-7',
+      type: 'error',
+      messageText: 'Something went wrong',
+      link: 'https://infor.com'
+    });
   }
 
 }
