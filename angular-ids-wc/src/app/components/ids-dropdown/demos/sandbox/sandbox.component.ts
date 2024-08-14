9ccb017e2a93b2462eee1e6ca2c47ca2c7be509f
Clay Clecidor <clay.clecidor@infor.com>
2024-08-14 14:47:35 -0400

    2612-ids-dropdown-dynamic-option-change

---
+++ angular-ids-wc/src/app/components/ids-dropdown/demos/sandbox/sandbox.component.ts
@@ -0,0 +1,39 @@
+import { Component, AfterViewInit, ViewChild, ElementRef, Input, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-sandbox-dropdown',
+  templateUrl: './sandbox.component.html',
+  styleUrls: ['./sandbox.component.css']
+})
+export class SandBoxComponent implements AfterViewInit {
+  @ViewChild('dropdownCar', { read: ElementRef }) dropdownCar;
+
+  constructor() { }
+
+  carOptions = [
+    { name: "volvo", id: 1, idx: "1718693262079751588" },
+    { name: "saab", id: 2, idx: "1718693262733452505" },
+    { name: "opel", id: 3, idx: "1718693263497253422" },
+    { name: "audi", id: 4, idx: "1718693264241054349" },
+  ];
+
+  ngAfterViewInit(): void {
+  }
+
+  selectedOption = signal(this.carOptions[2]);
+  sourceKey = signal("name");
+
+  onSelectionChange($event: string): void {
+    const newSelectedOption = this.carOptions.find((car) => car.idx === $event);
+    this.selectedOption.set(newSelectedOption);
+  }
+
+  changeSelected(value: string): void {
+    const nextCar = this.carOptions.find((car) => car.name === value);
+    this.selectedOption.set(nextCar);
+  }
+
+  changeSourceKey(sourceKey: string): void {
+    this.sourceKey.set(sourceKey);
+  }
+}
